1096b86126daf129b8088bb8eaee8c2a97d8e2fa
Jkose7 <ojosema10@gmail.com>
2024-09-25 14:32:40 -0500

    turbo config, eslint? ts?

---
+++ apps/web/.eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  root: true,
+  extends: ['@qualle/eslint-config/eslint.config.js'],
+};
---
+++ apps/web/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@qualle/ui"
+
+function App() {
+  return (
+    <>
+      <h1>Qualle</h1>
+      <Button></Button>
+    </>
+  )
+}
+
+export default App
