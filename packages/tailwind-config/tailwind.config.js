12169bc9ea1ed72a6fb3241c8428262971e1dae7
Jkose7 <ojosema10@gmail.com>
2024-09-25 21:04:24 -0500

    colors to tailwind.config.js

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -6,6 +6,14 @@ module.exports = {
     "../../packages/ui/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
+    colors: {
+      'dark': '#0a0a0a',
+      'light': '#ffffff',
+      'dark-gray': '#363434',
+      'light-gray': '#d9d9d9',
+      'purple': '#8561e2',
+      'dark-purple': '#49357C'
+    },
     extend: {},
   },
   plugins: [],
